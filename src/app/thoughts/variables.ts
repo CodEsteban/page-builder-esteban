adeaa030c7aa2203062114200f62fda77a6f611e
me <me@hello.com>
2022-11-06 17:33:28 -0500

    fixed android view thought details

--- src/app/thoughts/variables.ts
+++ src/app/thoughts/variables.ts
@@ -1,2 +1,3 @@
 export const localThought = "http://thought:3001";
-export const remoteThought = "http://esteban.lol";
+export const remoteThought = "http://192.168.1.62";
+//export const remoteThought = "http://localhost";
